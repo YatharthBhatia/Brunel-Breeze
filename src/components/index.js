63c42b5630aa0ef56e710d0d42ea4ac40ceb47e3
YatharthBhatia <yatharthbhatia2004@gmail.com>
2024-05-08 16:57:55 +0530

    Merge: exit

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-import Footer from "./footer";
-import Hero from "./hero";
-import Congratulations from "./congratulations";
-import Registration from "./registration";
-import { AccordionDemo } from "./faqs";
-
-export {Footer, Hero, AccordionDemo, Congratulations, Registration};
