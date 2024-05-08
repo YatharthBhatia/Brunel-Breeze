d3e109b9407e22efdd472e546e6a5f122541af31
YatharthBhatia <yatharthbhatia2004@gmail.com>
2024-05-08 17:18:10 +0530

    FAQs

--- src/data/faq.js
+++ src/data/faq.js
@@ -1,26 +1,26 @@
 const questions = [
   {
-    question: "Do you support freelancers?",
+    question: "Do you offer freelancers?",
     answer:
       "Yes, we provide comprehensive support for freelancers, ensuring smooth project execution and client satisfaction.",
   },
   {
-    question: "What assurances do you provide for client satisfaction?",
+    question: "What’s the guarantee that I will be satisfied with the hired talent?",
     answer:
       "Client satisfaction is our priority. We facilitate communication, allow revisions, and provide support for dispute resolution.",
   },
   {
-    question: "Can I hire multiple talents simultaneously?",
+    question: "Can I hire multiple talents at once?",
     answer:
       "Absolutely! Our platform allows you to hire multiple talents simultaneously, ensuring flexibility and efficiency.",
   },
   {
-    question: "What are the advantages of using your platform over agencies?",
+    question: "Why should I not go to an agency directly?",
     answer:
       "Our platform offers a diverse pool of talented freelancers, transparent communication, and mediation services.",
   },
   {
-    question: "Who can assist me in selecting the right talent?",
+    question: "Who can help me pick a right skillset and duration for me?",
     answer:
       "Our platform provides resources and support to help you choose the right talent for your project.",
   },
